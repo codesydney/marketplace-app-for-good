fdeb37bff7dfbb1d4b17b84dda9bda674a7f5c02
David Taing <dave@teeang.net>
2024-03-05 23:46:43 +1100

    fix: lint errors
    fix unescapted apostrophies introduced by PR #9

--- src/components/tutorial/SignUpUserSteps.tsx
+++ src/components/tutorial/SignUpUserSteps.tsx
@@ -13,8 +13,8 @@ export default function SignUpUserSteps() {
           >
             Login
           </Link>{" "}
-          page and sign up your first user. It's okay if this is just you for
-          now. Your awesome idea will have plenty of users later!
+          page and sign up your first user. It&apos;s okay if this is just you
+          for now. Your awesome idea will have plenty of users later!
         </p>
       </Step>
     </ol>
