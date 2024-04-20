31034a5233be954ac24e100c3a519d21ed3dd0bd
washington-shoji <shoji.washington@gmail.com>
2024-04-20 12:08:37 +1000

    - updated auth button component with additional navigation links

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -14,7 +14,7 @@ export default async function AuthButton() {
 
     const supabase = createClient()
     await supabase.auth.signOut()
-    return redirect('/login')
+    return redirect('/sign-in')
   }
 
   return user ? (
@@ -25,13 +25,41 @@ export default async function AuthButton() {
           Logout
         </button>
       </form>
+      <button className="bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline">
+        <Link href="/dashboard" className=" no-underline">
+          Dashboard
+        </Link>
+      </button>
     </div>
   ) : (
-    <Link
-      href="/login"
-      className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
-    >
-      Login
-    </Link>
+    <div className="flex gap-2">
+      <Link
+        href="/sign-in"
+        className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+      >
+        Login
+      </Link>
+
+      <Link
+        href="/tasks"
+        className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+      >
+        Tasks
+      </Link>
+
+      <Link
+        href="/my-tasks"
+        className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+      >
+        My Tasks
+      </Link>
+
+      <Link
+        href="/my-offers"
+        className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+      >
+        My Offers
+      </Link>
+    </div>
   )
 }
