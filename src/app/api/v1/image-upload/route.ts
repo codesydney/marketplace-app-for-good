2d0fd3c1b56284321d0a9c97d88f1f420b0c6e5a
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-24 10:57:17 +1000

    MA5 - finalising api router for image upload

--- src/app/api/v1/image-upload/route.ts
+++ src/app/api/v1/image-upload/route.ts
@@ -9,13 +9,38 @@ const s3Client = new S3Client({
   },
 })
 
-export const POST = (req: Request) => {
-  console.info(PutObjectCommand)
-  console.info(req)
-  console.info(s3Client)
+const uploadFileToS3 = async (buffer: Buffer, fileName: string) => {
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME as string,
+    Key: `${fileName}-${Date.now()}`,
+    Body: buffer,
+  })
 
-  return NextResponse.json(
-    { success: true, message: 'Hello, World!' },
-    { status: 200 },
-  )
+  const response = await s3Client.send(command)
+  return response
+}
+
+export const POST = async (req: Request) => {
+  try {
+    const formData = await req.formData()
+    const file = formData.get('file') as File
+
+    if (!file) {
+      return NextResponse.json(
+        { success: false, message: 'File is required' },
+        { status: 400 },
+      )
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer())
+    const fileName = await uploadFileToS3(buffer, file.name)
+
+    return NextResponse.json({ success: true, fileName }, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { success: false, message: 'Error uploading image' },
+      { status: 500 },
+    )
+  }
 }
