216d72c6368c2ce0d0b5b859600199a94f48ec40
David Taing <dave@teeang.net>
2024-05-31 14:32:34 +1000

    feat: add stricter input validation for abn & acn
    Strip all spaces from ABNs & ACNs and checks for numeric characters only

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -15,9 +15,19 @@ export const serviceProviderSignupFormSchema = zfd.formData({
   password: zfd.text(z.string().min(8)),
   preferred_name: zfd.text(z.string()),
   fullname: zfd.text(z.string()),
-  company_name: zfd.text(z.string()),
-  abn: zfd.text(z.string().length(11)),
-  acn: zfd.text(z.string().length(9).optional()),
+  company_name: zfd.text(z.string().trim()), // Add .trim() to remove leading and trailing whitespace
+  abn: zfd.text(
+    z
+      .string()
+      .transform(s => s.replace(/\s/g, ''))
+      .pipe(z.string().length(11).regex(/^\d+$/)),
+  ),
+  acn: zfd.text(
+    z
+      .string()
+      .transform(s => s.replace(/\s/g, ''))
+      .pipe(z.string().length(9).regex(/^\d+$/).optional()),
+  ),
   industry: zfd.text(z.string()),
 })
 
