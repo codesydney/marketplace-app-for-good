63f1dba2e863b5f635a162e280322879ed0707c3
David Taing <dave@teeang.net>
2024-05-31 15:16:53 +1000

    fix: "create an account" button url

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -82,7 +82,7 @@ export default function Login({
                 </Box>
 
                 <Flex direction="row" width="100%" justify="end" gap="4">
-                  <Link href="/service-providers/sign-up">
+                  <Link href="/sign-up">
                     <Button type="button" color="indigo" variant="soft">
                       Create an account
                     </Button>
