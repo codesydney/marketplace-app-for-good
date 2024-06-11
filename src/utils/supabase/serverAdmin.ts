427080c1bdb82d30ad50ba764a59f9998c428b4e
David Taing <dave@teeang.net>
2024-06-11 14:35:23 +1000

    refactor: move supabase admin client to src/utils

--- src/utils/supabase/serverAdmin.ts
+++ src/utils/supabase/serverAdmin.ts
@@ -1,11 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { Database } from '@/types/supabase'
+
 /**
  * Supabase client with the Service role.
  * This role has the ability to bypass Row Level Security.
  * Use this client wisely and never expose it to the client side.
  */
-export const supabaseAdminClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
-)
+export const createAdminClient = () =>
+  createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+  )
