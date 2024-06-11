9e87071e394d5746df8b62fbc2a0fa89b8be4178
David Taing <dave@teeang.net>
2024-06-11 18:49:36 +1000

    build: load test environment variables in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { loadEnvConfig } from '@next/env'
+
+loadEnvConfig(process.cwd())
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
