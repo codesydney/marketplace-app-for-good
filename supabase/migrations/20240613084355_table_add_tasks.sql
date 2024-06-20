655d3f1122e0e22cb732346144525a45f58e0b60
David Taing <dave@teeang.net>
2024-06-20 16:16:27 +1000

    migrations: fix incorrect tasks schema
    This commit fixes several typos in the table_add_tasks migration file

--- supabase/migrations/20240613084355_table_add_tasks.sql
+++ supabase/migrations/20240613084355_table_add_tasks.sql
@@ -4,13 +4,13 @@ create type task_due_date_type_enum AS ENUM ('ON_DATE', 'BEFORE_DATE');
 create table tasks (
   id uuid primary key default gen_random_uuid(),
   customer_id uuid references auth.users(id) not null,
+  address_id uuid references addresses(id) not null,
+  title text not null,
+  description text not null,
   budget numeric not null,
-  title task_status_enum not null default 'OPEN',
-  status text not null,
+  status task_status_enum not null default 'OPEN',
   due_date date not null,
   due_date_type task_due_date_type_enum not null default 'BEFORE_DATE',
-  description text not null,
-  address_id uuid references addresses(id) not null,
   created_at timestamptz default now(),
   updated_at timestamptz default now()
 );
