aaca049ae20951fa823f48ff957978ef0662491f
David Taing <dave@teeang.net>
2024-06-21 00:41:56 +1000

    fix: policy table

--- supabase/migrations/20240620112550_policy_add_task_categories_permissions.sql
+++ supabase/migrations/20240620112550_policy_add_task_categories_permissions.sql
@@ -1,6 +1,6 @@
 alter table task_categories enable row level security;
 
 create policy "anyone can view task_categories"
-on tasks for select
+on task_categories for select
 to authenticated, anon
 using ( true );
