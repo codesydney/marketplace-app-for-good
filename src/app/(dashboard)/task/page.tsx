24717700c7fac674950d81cf5be068ed93dcbf37
David Taing <dave@teeang.net>
2024-06-21 12:07:43 +1000

    WIP feat: fetch list of customer tasks on Tasks pages

--- src/app/(dashboard)/task/page.tsx
+++ src/app/(dashboard)/task/page.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import React from 'react'
 import { TextField, Heading, Table, Badge } from '@radix-ui/themes'
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { useQuery } from '@tanstack/react-query'
+
 import { formatDate } from '@/utils/utils'
 import HeaderInfoCard from '@/components/header-info-card/HeaderInfoCard'
 import FilterSelector from '@/components/filter-selector/FilterSelector'
 import DashboardTable from '@/components/dashboard-table/DashboardTable'
 import TableActionMenu from '@/components/table-action-menu/TableActionMenu'
+
+import { createClient } from '@/utils/supabase/client'
 
 // TODO: move the type definitions to it's own folders/files
 type TaskHeaderCard = {
@@ -43,25 +49,39 @@ const status: FilterOption = {
   ],
 }
 
-const newDate = new Date().toISOString()
-const tableData = [
-  {
-    id: 123456,
-    title: 'task name 1',
-    status: 'open',
-    date: newDate,
-    location: 'sydney st 100',
-  },
-  {
-    id: 654321,
-    title: 'task name 2',
-    status: 'completed',
-    date: newDate,
-    location: 'sydney st 200',
-  },
-]
+async function getCustomerTasks() {
+  const supabase = createClient()
+  const {
+    data: { session },
+    error: sessionError,
+  } = await supabase.auth.getSession()
 
-export default function Task() {
+  const userId = session?.user?.id
+
+  if (sessionError || !userId) {
+    return null
+  }
+
+  const { data } = await supabase
+    .from('tasks')
+    .select('*')
+    .eq('customer_id', userId)
+
+  return data
+}
+
+function useCustomerTasksQuery() {
+  const query = useQuery({
+    queryKey: ['tasks'],
+    queryFn: getCustomerTasks,
+  })
+
+  return query
+}
+
+export default function TasksPage() {
+  const { data: tasks } = useCustomerTasksQuery()
+
   return (
     <div className="flex flex-col gap-2 px-4">
       <Heading className="my-2">Tasks</Heading>
@@ -83,32 +103,27 @@ export default function Task() {
 
       <div>
         <DashboardTable
-          headers={[
-            'task id',
-            'title',
-            'status',
-            'date',
-            'location',
-            'actions',
-          ]}
+          headers={['task', 'title', 'location', 'date', 'status', 'actions']}
         >
-          {tableData.map((cell, index) => {
+          {tasks?.map((cell, index) => {
             return (
               <Table.Row key={index}>
                 <Table.RowHeaderCell>{cell.id}</Table.RowHeaderCell>
                 <Table.Cell>{cell.title}</Table.Cell>
                 <Table.Cell>
+                  {cell.suburb}&nbsp;{cell.postcode}
+                </Table.Cell>
+                <Table.Cell>{formatDate(cell.due_date.toString())}</Table.Cell>
+                <Table.Cell>
                   <Badge
                     variant="solid"
                     radius="full"
                     color="indigo"
-                    className=" p-2"
+                    className="p-2 lowercase"
                   >
                     {cell.status}
                   </Badge>
                 </Table.Cell>
-                <Table.Cell>{formatDate(cell.date.toString())}</Table.Cell>
-                <Table.Cell>{cell.location}</Table.Cell>
                 <Table.Cell>
                   <TableActionMenu label="options" />
                 </Table.Cell>
